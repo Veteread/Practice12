1bc04aa1c4db6dcd165a5958ecb9f5719249f265
Veteread <veteran23@mail.ru>
2023-08-16 19:39:23 +0300

    add destroy ParticleSystem

--- Practice11-main/Assets/Scripts/Bonus.cs
+++ Practice11-main/Assets/Scripts/Bonus.cs
@@ -5,14 +5,25 @@ using static UnityEngine.ParticleSystem;
 
 public class Bonus : MonoBehaviour
 {
-    public GameObject BonusObj;
+    public MeshRenderer BonusObj;
     public ParticleSystem BurstBonus;
+
+    private void Awake()
+    {
+        BonusObj = GetComponent<MeshRenderer>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            BonusObj.SetActive(false);            
-            BurstBonus.Play(true);            
+            BurstBonus.Play(true);
+            BonusObj.enabled = false;                  
+            Invoke("Del", 2f);
         }
+    }
+
+    private void Del()
+    {
+      ParticleSystem.Destroy(BurstBonus, 1f); 
     }
 }
